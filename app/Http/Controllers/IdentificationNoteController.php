a06f9889fb549a7c9e73dead8cd62f777b40fe92
Eduardo <eromero@fiabilis.cl>
2022-02-27 13:26:08 -0300

    show de identification_note lista

--- app/Http/Controllers/IdentificationNoteController.php
+++ app/Http/Controllers/IdentificationNoteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use app\Models\IdentificationNote;
+use App\Models\IdentificationNote;
 
 class IdentificationNoteController extends Controller
 {
--- resources/views/models/identification_note/show.blade.php
+++ resources/views/models/identification_note/show.blade.php
@@ -1,1 +1,58 @@
-en construccion
+@extends('listado')
+
+
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+
+                <div class="row">
+                    <div class="col-12">
+                        <h4 class="my-0">ID_IDENTIFICATION_NOTE: {{ $identification_note->ID_IDENTIFICATION_NOTE }}</h4>
+                        <p>Tabla <b>identification_note</b></p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    @foreach (config('tablas') as $table_name => $columns)
+                        @if ( $table_name === "identification_note" )
+                            @foreach ($columns as $column)
+                                <div class="col-3 form-group">
+                                    <label>{{ $column }}</label>
+                                    <input id="{{ $column }}" type="text" class="form-control" value="{{ $identification_note->{$column} }}" disabled title="{{ $identification_note->{$column} }}">
+                                </div>
+                            @endforeach
+                        @endif
+                    @endforeach
+
+                </div>
+
+                @if ( $identification_note->identification )
+                    <div class="row">
+                        <div class="col-12">
+                            <p><b>Tabla Identification</b></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        @foreach (config('tablas')['identification'] as $column)
+                            <div class="col-3 form-group">
+                                <label>{{ $column }}</label>
+                                <input id="{{ $identification_note->identification->{$column} }}" type="text" class="form-control" value="{{  $identification_note->identification->{$column} }}" disabled title="{{ $identification_note->identification->{$column} }}">
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+
+
+
+
+            </div>
+
+
+
+        </div> <!-- End card -->
+    </div>
+</div>
+@endsection
